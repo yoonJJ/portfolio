bf1d13c797cdb28ea598f14abdac010742db8266
yoonJJ <bubu_@kakao.com>
2025-06-26 16:50:46 +0900

    first commit

---
+++ src/main/java/com/portfolio/jjyoon/JjyoonApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jjyoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjyoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjyoonApplication.class, args);
+	}
+
+}
